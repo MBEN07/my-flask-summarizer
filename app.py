115ed3c46c1751fea7bd58fadc3146ef1e348a74
MOHAMED BENTALB <135471875+MBEN07@users.noreply.github.com>
2025-07-01 04:35:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def get_test_samples():
 
 # -------- Run the app --------
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
